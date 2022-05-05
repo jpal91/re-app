9f9343b110eb35929c491c9abd1ec15bf6518ca3
jpal91 <98477057+jpal91@users.noreply.github.com>
2022-05-05 16:12:38 -0400

    added env variable

--- src/actions/api.js
+++ src/actions/api.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
+require("dotenv").config();
 
 const api = axios.create({
   baseURL: "https://us-real-estate.p.rapidapi.com",
   headers: {
     "X-RapidAPI-Host": "us-real-estate.p.rapidapi.com",
-    "X-RapidAPI-Key": "942d526276msh2327b24f85690a7p13e988jsn34090db0fc48"
+    "X-RapidAPI-Key": process.env.USRE_API
   }
 });
 
